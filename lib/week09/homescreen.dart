426b861c3b851884134d48ba5413cefafba891c8
lellon0403 <a67733198@gmail.com>
2025-10-30 15:08:38 +0900

    Updated Changes

--- lib/week09/homescreen.dart
+++ lib/week09/homescreen.dart
@@ -10,12 +10,16 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreenState extends State<HomeScreen> {
   
+  int click =1;
+
   Widget build(BuildContext context){
     return Scaffold(
       body: Column(
         children: [
-          if( click = 1){
+          if( click ==  1){
           Text("안녕하세요")
+          } else if( click == 2) {
+            Text("반가워요"),
           }
         ],
   
